5666751799ba746225a0f27289318798fafece7d
megangoodland <megangoodland@gmail.com>
2019-03-03 16:58:13 -0500

    Update rarray_math.h

--- rarray_math.h
+++ rarray_math.h
@@ -1,32 +1,27 @@
+// rarray_math.h
+// Header for the module for doing math functions on rarrays for GW_detection.cc
+
 #ifndef rarray_math_h
 #define rarray_math_h
 
-void function();
 
-
-//#include <iostream>
 #include <rarray>
-//#include <rarrayio>
-//#include <vector>
-//#include <netcdf>
 #include <complex>
-//#include <fftw3.h>
-//#include <cmath> // for pow
-//#include "netCDF_reading.h"
-//#include "rarray_math.h"
-//#include <cblas.h>
-//using namespace std;
-//using namespace netCDF;
 
-// Fast fourier transform function: performs a fast fourier transform.
-// Input: f rarray
-// Output: fhat rarray
+
+// fft: Fast fourier transform function for 1D complex double rarray
+//      Input: f rarray
+//      Output: fhat rarray
 rarray<std::complex<double>,1> fft(rarray<std::complex<double>,1>& f);
 
-// This function returns an rarray of the square norms of the complex values in the rarray input
+// sq_norm: This function returns an rarray of the square norms of the complex values in the rarray input
+//      Input: 1D complex double rarray
+//      Output: 1D double rarray
 rarray<double,1> sq_norm(rarray<std::complex<double>,1>& fhat);
 
-// This function calculates the correlation between two 1D rarrays via C_FG = <F,G> / sqrt(<F,F><G,G>)
+// correlation: This function calculates the correlation between two 1D rarrays via C_FG = <F,G> / sqrt(<F,F><G,G>)
+//      Input: Two 1D double rarrays
+//      Output: A double value
 double correlation(rarray<double,1>& Fk,rarray<double,1>& Gk);
 
 
