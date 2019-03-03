0bf59dbda69a7283c1d33fcb62aa60701f3d7a0f
megangoodland <megangoodland@gmail.com>
2019-03-02 20:15:12 -0500

    Removed netCDF functions to put them in their own module
    And added #include "netCDF_reading.h"

--- GW_detection.cc
+++ GW_detection.cc
@@ -27,27 +27,9 @@
 #include <netcdf>
 #include <complex>
 #include <fftw3.h>
+#include "netCDF_reading.h"
 using namespace std;
 using namespace netCDF;
-
-// This function reads f from the file and returns it in an rarray
-// Input: name of the file
-// Output: 1D complex double rarray
-rarray<complex<double>,1> get_f(string s){
-  NcFile file(s, NcFile::read); // selects the file to read
-  rarray<complex<double>,1> f(file.getDim("nt").getSize()); // defining rarray called f to hold the data
-  file.getVar("f").getVar(&f[0]); // putting data in rarray
-  return f;
-}
-
-// This function returns the size of variable f from a netCDF file
-int get_f_size(string s){
-  NcFile file(s, NcFile::read); // selects the file to read
-  rarray<complex<double>,1> f(file.getDim("nt").getSize()); // defining rarray called f to hold the data
-  file.getVar("f").getVar(&f[0]); // putting data in rarray
-  int f_size = f.extent(0);
-  return f_size;
-}
 
 // Fast fourier transform function: performs a fast fourier transform.
 // Input: f rarray
